71b75f9e539d910442cdb017f6ee9422871bd6a6
vivzhang <vivian.mzh@gmail.com>
2016-11-11 22:04:06 -0800

    Displays messages on DOM

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,20 +1,16 @@
 // YOUR CODE HERE:
 
-  var message = {
-    username: 'Vivian',
-    text: 'Holaaa!!!',
-    roomname: 'lobby'
-  };
 
   const app = {};
   app.init = () => {
-
+    app.server = 'https://api.parse.com/1/classes/messages';
+    app.fetch();
   };
 
   app.send = (messages) => {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
-      url: 'https://api.parse.com/1/classes/messages',
+      url: app.server,
       type: 'POST',
       data: JSON.stringify(messages),
       contentType: 'application/json',
@@ -26,15 +22,19 @@
         console.error('chatterbox: Failed to send message', data);
       }
     });
+    app.fetch();
   };
 
   app.fetch = () => {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
-      // url: 'https://api.parse.com/1/classes/messages',
+      url: app.server,
       type: 'GET',
+      date: 'order=-createdAt',
+      contentType: 'application/json',
       success: function (data) {
-        console.log(data);
+        data.results.forEach(app.renderMessage);
+        console.log('chatterbox: Message received');
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -47,9 +47,35 @@
     $('#chats').empty();
   };
 
-  app.renderMessage = (message) => {
-    var $message = '<div>' + message + '</div>';
-    $('#chats').append($message);
+  app.createMessage = () => {
+    return {
+      username: window.location.search.slice(10),
+      text: $('.inputMessage').val()
+    };
   };
+
+  app.renderMessage = (result) => {
+    $('#chats').append(`<div><p><a href='#'>${result.username}:</a><br>${result.text}</p></div>`);
+  };
+
+  app.renderRoom = (newRoom) => {
+    $('#roomSelect').append(`<option> + ${newRoom} + </option>`);
+  };
+
+  $('#main').on('click', '.submitBtn', () => {
+    app.send();
+  });
+
+  // app.createMessage = () => {
+  //   return message = {
+  //     username: 'Vivian',
+  //     text: 'Holaaa!!!',
+  //     roomname: 'lobby'
+  //   };
+  // }; 
+
+  app.init();
+
+
 
 
